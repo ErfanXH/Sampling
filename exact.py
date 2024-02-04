8465485c48ce5d2523c0a65c4aeae92f7375c008
Erfan Hemati <efnhmt2020@gmail.com>
2024-02-04 15:48:06 +0330

    Exact-Inference Implemented

---
+++ exact.py
@@ -0,0 +1,18 @@
+import utils
+
+def exact_inference(query, evidence, cpts, graph):
+    new_cpts = []
+    parents = graph['parents_nodes']
+    for i, cpt in enumerate(cpts):
+        tb = []
+        for row in cpt:
+            if evidence.get(i) != None and evidence[i] != row[i]: 
+                continue
+            flag = True
+            for j in parents[i]:
+                if evidence.get(j) and row[j] != evidence[j]:
+                    flag = False
+            if flag:
+                tb.append(row)
+        new_cpts.append(tb)
+    return variable_elimination(evidence, query, new_cpts)
